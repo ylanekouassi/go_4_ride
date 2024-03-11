c6603432498c646f98ecdb79d8406ebcfa6adec1
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-10 23:28:08 -0400

    car is displaying in the window

--- go_4_ride.py
+++ go_4_ride.py
@@ -3,4 +3,10 @@
 from tkinter import *
 
 window = Tk()
-window.geometry() #size of the window
+window.geometry("500x200") #size of the window
+
+car = PhotoImage(file="ride.png")
+label = Label(window, image=car)
+label.place(x=0, y=0)
+
+window.mainloop()
