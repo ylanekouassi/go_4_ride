f3d225d208c5f2d6a76754d42340560a060034c9
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-11 12:18:36 -0400

    car is going to the right direction

--- go_4_ride.py
+++ go_4_ride.py
@@ -2,9 +2,18 @@
 
 from tkinter import *
 
+def go_down(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y()+5)
+
+
 window = Tk()
 window.geometry("1000x250") #size of the window
 window.title("BLACK CAR")
+
+window.bind("<Down>", go_down)
+window.bind()
+window.bind()
+window.bind()
 
 car = PhotoImage(file="car.png")
 label = Label(window, image=car)
