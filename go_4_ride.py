a9bc53efbbde03e7684b7e5e620f06e04205519a
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-11 12:33:28 -0400

    the car is going up, down, left and right

--- go_4_ride.py
+++ go_4_ride.py
@@ -5,15 +5,24 @@ from tkinter import *
 def go_down(event):
     label.place(x=label.winfo_x(), y=label.winfo_y()+5)
 
+def go_up(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y()-5)
+
+def reverse(event):
+    label.place(x=label.winfo_x()-5, y=label.winfo_y())
+
+def accelerate(event):
+    label.place(x=label.winfo_x()+5, y=label.winfo_y())
+
 
 window = Tk()
 window.geometry("1000x250") #size of the window
 window.title("BLACK CAR")
 
 window.bind("<Down>", go_down)
-window.bind()
-window.bind()
-window.bind()
+window.bind("<Up>", go_up)
+window.bind("<Left>", reverse)
+window.bind("<Right>", accelerate)
 
 car = PhotoImage(file="car.png")
 label = Label(window, image=car)
