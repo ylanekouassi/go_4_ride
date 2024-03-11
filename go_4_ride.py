52dc65cbe6892b845522e069bcb2f8288d522902
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-10 23:50:58 -0400

    image updated

--- go_4_ride.py
+++ go_4_ride.py
@@ -1,11 +1,12 @@
-#LET'S GO FOR RIDE
+#LET'S GO FOR A RIDE
 
 from tkinter import *
 
 window = Tk()
-window.geometry("500x200") #size of the window
+window.geometry("1000x250") #size of the window
+window.title("BLACK CAR")
 
-car = PhotoImage(file="ride.png")
+car = PhotoImage(file="car.png")
 label = Label(window, image=car)
 label.place(x=0, y=0)
 
